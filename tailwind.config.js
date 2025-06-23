e376c55a7a9e46f7f304bca2c4e0994f98ca870d
CodingDango <babyyodaman2.0@gmail.com>
2025-06-23 23:28:09 +0800

    First ever commit

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: [
+    './docs/*.html', 
+  ],
+
+  theme: {
+    extend: {
+      colors: {
+        'my-black-300'  : '#121212',
+        'my-black-250'  : '#1E1E1E',
+        'my-black-200'  : '#2C2C2C',
+        'my-black-100'  : '#464646',
+        'my-neutral-max': '#171717',
+        'my-white'      : '#EFEEEA',
+        'my-orange'     : '#FE7743',
+        'neon-purple'   : '#9370DB',
+        'my-light-gray' : '#99A1AF',
+        'my-lime' : '#B8DB70',
+      },
+
+      fontFamily: {
+        'sans': ['"Roboto"', fontFamily.sans],
+        'roboto-mono': ['"Roboto Mono"', fontFamily.sans],
+      },
+      screens: {
+        'xs': '486px'
+      }
+    },
+  },
+}
+
+
